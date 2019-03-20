4e95c7ff77e124b18bb534f0fe26ab31998d6fc9
Hamza ben romdhane <hamzabnromdhane@gmail.com>
2019-03-20 23:07:28 +0100

    add command model

---
+++ src/models/command.js
@@ -0,0 +1,46 @@
+import { List, Field } from 'keystone';
+
+const schemaName = 'Command';
+const schemaData = {
+	user: {
+		type: Field.Types.Relationship, ref: 'User',
+		required: true,
+		initial: true
+	},
+	space: {
+		type: Field.Types.Relationship, ref: 'Space',
+		required: true,
+		initial: true
+	},
+	mealsList: {
+		type: Field.Types.Relationship, ref: 'Meal',
+		many: true
+	},
+	state: { type: Field.Types.Select, options: 'PASSED, COOKING, LIVRED', default: 'PASSED' },
+	price: {
+		type: Number,
+	}
+}
+
+const schemaKeystone = new List(schemaName, {
+	map: { name: 'name' },
+	searchFields: 'space name price',
+	plural: 'commands',
+	singular: 'command'
+});
+
+schemaKeystone.add(schemaData);
+schemaKeystone.defaultColumns = 'name, space, price';
+
+schemaKeystone.track = {
+	createdAt: true,
+	updatedAt: true,
+	createdBy: true,
+	updatedBy: true
+};
+
+// todo : add pre save hook to check email and other data
+
+schemaKeystone.register();
+
+export default schemaKeystone.model;
